fb9efc86f45d185276f350f1347fa3042fa32f6a
omojibola <omojibolaorija@gmail.com>
2021-01-03 01:58:13 +0100

    changed data from json to js file

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
-import deskHeader from './images/deskHeader.svg'
-import data from './data.json';
+import deskHeader from './images/deskHeader.svg';
+import { data } from './data';
 import Display from './itemDisplay';
 
-
-
-const App =() => {
+const App = () => {
   const [items, setItems] = useState(data);
 
-
-
-
   return (
-    <div className='App'>
+    <div className="App">
       <header>
-      <img src ={deskHeader} alt='header-image' className='deskHeader'/>
+        <img src={deskHeader} alt="header-image" className="deskHeader" />
       </header>
-      <div className='item-list'>
+      <div className="item-list">
         {items.map((result, index) => (
-          <div className='item-data'>
-          <Display result ={result} index={index} />
+          <div className="item-data">
+            <Display result={result} index={index} />
           </div>
         ))}
       </div>
-
     </div>
-  )
-}
-
-
+  );
+};
 
 export default App;
-
-
-
-
-
-
-
